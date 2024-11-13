7331fc48e00c66c2511e8db256c87a7ba0af7b3c
Suhas Thalanki <suhastvs@gmail.com>
2024-11-13 14:35:31 -0500

    added basic cli

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
+use clap::Parser;
+
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None)]
+struct Args {
+    // Input File Path
+    #[arg(short, long, required = true)]
+    input_file_path: String,
+    // Output File Path
+    #[arg(short, long, required = false)]
+    output_file_path: Option<String>,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = Args::parse();
+    println!("Input File: {:?}, Output File: {:?}", args.input_file_path, args.output_file_path);
 }
